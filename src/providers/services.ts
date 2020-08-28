55ff1a7e03f4ad6d5028ec2101b485a895b24122
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-08-28 11:11:55 -0700

    web pusher ready for very basic testing

--- src/providers/services.ts
+++ src/providers/services.ts
@@ -1,6 +1,8 @@
 class Services {
     listener(message: any, cb: (ok: boolean) => void): void {
         console.log('Got msg', message.content.toString());
+        const content = JSON.parse(message.content.toString())
+        console.log(content['message']);
         cb(true);
     }
 }
